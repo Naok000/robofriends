d4e9ca1b5a5e5c1c8c47fa4c9496a3cfd9a02df6
Naoki Kojima <dark.light9640@gmail.com>
2020-06-17 19:25:32 +0900

    fixed code

--- src/components/ErrorBoundry.js
+++ src/components/ErrorBoundry.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-class ErrorBoundry extends Comment {
+class ErrorBoundry extends Component {
     constructor(props) {
         super(props);
         this.state = {
